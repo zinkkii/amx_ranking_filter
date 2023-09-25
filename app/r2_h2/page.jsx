e1adc4078344a1b79b2b7fe1dacda50a22792301
zinkkii <zinkkiiiii@gmail.com>
2023-09-26 05:05:28 +0900

    reload update

--- app/r2_h2/page.jsx
+++ app/r2_h2/page.jsx
@@ -1,7 +1,14 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import Layout from "../layouts/layout";
 import Result_R2_H2 from "./Result_R2_H2";
 
 export default function R2_H2() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/r2_h2");
+  }, []);
   return (
     <Layout>
       <Result_R2_H2 />
--- app/r2_q/page.jsx
+++ app/r2_q/page.jsx
@@ -1,7 +1,14 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import Layout from "../layouts/layout";
 import Result_R2_Q from "./Result_R2_Q";
 
 export default function R2_Q() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/r2_q");
+  }, []);
   return (
     <Layout>
       <Result_R2_Q />
